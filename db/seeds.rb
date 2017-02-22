7e6c2729685c2392895579dd7ff81d33efc35654
Anton Kovalenko <shmdtt@gmail.com>
2017-02-22 23:35:50 +0200

    refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-News.create!(
-  title: 'title',
-  mobile_image: open("#{Rails.root}/app/assets/images/logo_old.png"),
-  website_image: open("#{Rails.root}/app/assets/images/logo_old.png")
-)
-News.create!(
-  title: 'title',
-  mobile_image: open("#{Rails.root}/app/assets/images/sellit.png"),
-  website_image: open("#{Rails.root}/app/assets/images/sellit.png")
-)
-News.create!(
-  title: 'title',
-  mobile_image: open("#{Rails.root}/app/assets/images/dinein.png"),
-  website_image: open("#{Rails.root}/app/assets/images/dinein.png")
-)
+#News.create!(
+#  title: 'title',
+#  mobile_image: open("#{Rails.root}/app/assets/images/logo_old.png"),
+#  website_image: open("#{Rails.root}/app/assets/images/logo_old.png")
+#)
+#News.create!(
+#  title: 'title',
+#  mobile_image: open("#{Rails.root}/app/assets/images/sellit.png"),
+#  website_image: open("#{Rails.root}/app/assets/images/sellit.png")
+#)
+#News.create!(
+#  title: 'title',
+#  mobile_image: open("#{Rails.root}/app/assets/images/dinein.png"),
+#  website_image: open("#{Rails.root}/app/assets/images/dinein.png")
+#)
+3.times do |n|
+  arr = [
+    "#{Rails.root}/app/assets/images/sellit.png",
+    "#{Rails.root}/app/assets/images/dinein.png",
+    "#{Rails.root}/app/assets/images/logo_old.png"
+  ]
+  News.create!(
+    title: 'title',
+    mobile_image: open(arr[(n + 1) - 1]),
+    website_image: open(arr[(n + 1) - 1])
+  )
+end
