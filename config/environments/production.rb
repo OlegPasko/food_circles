25a10b8229210d0810438301e2dd35ae80212545
Eugene Domotenko <e.domotenko@gmail.com>
2017-03-24 15:37:23 +0200

    fix env key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: 'foodcircles.net' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            'h@foodcircles.net',
+    password:             'muxmool25',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  config.mix_panel_api_key = 'b09b7e1d5dc4190f983cb3244066941a'
 end
